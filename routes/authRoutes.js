c0d25cf9277c64ee827b8638e656901c633c28e0
uca-spider <ucaspider@gmail.com>
2024-12-20 15:02:53 +0700

    fix error login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, protectedRoute } = require("../controllers/authController");
+const { register, login, protectedRoute  } = require("../controllers/authController");
 const { verifyToken } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/protected", verifyToken, protectedRoute);
+
 
 module.exports = router;
